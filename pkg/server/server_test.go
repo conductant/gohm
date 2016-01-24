1950edbf977ae9f6f655554b01ca73c1f372c9bb
David Chung <dchung@qoriolabs.com>
2016-01-23 20:41:59 -0800

    Add test case for checking error handler via context

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -44,6 +44,14 @@ func (this *testE2EServer) testSimpleGet(ctx context.Context, resp http.Response
 	return
 }
 
+var testServerError = testApi("/server-error", GET, "test-server-error")
+
+func (this *testE2EServer) testServerError(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+	this.c.Log("testServerError called.")
+	HandleError(ctx, http.StatusInternalServerError, "server-error")
+	return
+}
+
 type TestSuiteServer struct {
 	server     *testE2EServer
 	stopServer chan<- int
@@ -62,6 +70,7 @@ func (suite *TestSuiteServer) SetUpSuite(c *C) {
 		ListenPort(7890).
 		Route(testSimpleGet).To(suite.server.testSimpleGet).
 		Route(testGetApiFromContext).To(suite.server.testGetApiFromContext).
+		Route(testServerError).To(suite.server.testServerError).
 		Route(testAnonymousFunc).To(
 		func(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
 			c.Log("testAnonymousFunc called.")
@@ -126,5 +135,20 @@ func (suite *TestSuiteServer) TestWithAuthToken(c *C) {
 		},
 		func(resp *http.Response, body []byte) {
 			c.Assert(resp.StatusCode, Equals, http.StatusUnauthorized)
+		})
+}
+
+func (suite *TestSuiteServer) TestHandleError(c *C) {
+	token := auth.NewToken(1*time.Hour).Add("test-server-error", 1)
+
+	testutil.GetWithRequest(c, "http://localhost:7890/server-error",
+		func(req *http.Request) *http.Request {
+			token.SetHeader(req.Header, testutil.PrivateKeyFunc)
+			return req
+		},
+		func(resp *http.Response, body []byte) {
+			c.Log("resp=", resp.StatusCode, "body=", string(body))
+			c.Assert(resp.StatusCode, Equals, http.StatusInternalServerError)
+			c.Assert(body, DeepEquals, []byte(`{ "error": "server-error" }`))
 		})
 }
