37ae5c3cb4a735498ff9159585e00c2f76281a8f
David Chung <davidchung@Davids-MacBook-Pro-2.local>
2016-03-03 17:53:19 -0800

    Support for {{define}} blocks that are used as user variables: see test case; this is similar to Hashicorp's Packer support for the variables block in their packer.json format

--- pkg/template/template.go
+++ pkg/template/template.go
@@ -2,11 +2,12 @@ package template
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/conductant/gohm/pkg/resource"
 	"golang.org/x/net/context"
+	"gopkg.in/yaml.v2"
 	"hash/fnv"
 	"strconv"
-	"strings"
 	"text/template"
 )
 
@@ -46,6 +47,89 @@ func Apply(tmpl []byte, data interface{}, funcs ...template.FuncMap) ([]byte, er
 	return buff.Bytes(), err
 }
 
+/*
+Two-pass apply.  This supports the notion of a define template block where the content is
+in YAML. The content in YAML is then parsed and in the second pass any template expressions that
+calls the function {{my <var>}} will access the values in the define blocks.
+
+For example:
+
+{{define "app"}}
+version: 1.2
+image: repo
+build: 1234
+{{end}}
+
+{{define "host"}}
+label: appserver
+name: myhost
+{{end}}
+
+{
+   "image" : "repo/myapp:{{my app.version}}-{{my app.build}}",
+   "host" : "{{my host.name}}"
+}
+
+Will turn into:
+
+{
+   "image" : "repo/myapp:1.2-1234",
+   "host" : "myhost"
+}
+*/
+func Apply2(tmpl []byte, data interface{}, funcs ...template.FuncMap) ([]byte, error) {
+	fm := template.FuncMap{}
+	for _, opt := range funcs {
+		fm = MergeFuncMaps(fm, opt)
+	}
+
+	// support for my variables via the define block:
+	// first pass just escape all the expressions
+	fm["my"] = func(v string) string {
+		return "{{my '" + v + "'}}"
+	}
+
+	t := template.New(GetKeyForTemplate(tmpl)).Funcs(fm)
+	t, err := t.Parse(string(tmpl))
+	if err != nil {
+		return nil, err
+	}
+
+	vars := make(map[string]interface{})
+	for _, ct := range t.Templates() {
+		body := new(bytes.Buffer)
+		err := ct.Execute(body, nil)
+		if err != nil {
+			return nil, err
+		}
+		// attempt to parse as YAML.  It's possible that
+		// the template may be something else.
+		m := make(map[string]interface{})
+		if err := yaml.Unmarshal(body.Bytes(), m); err == nil {
+			for k, v := range m {
+				vars[ct.Name()+"."+k] = v
+			}
+		}
+	}
+
+	// Second pass - now we do not escape the my function.
+	fm["my"] = func(k string) string {
+		if v, has := vars[k]; has {
+			switch v := v.(type) {
+			case string:
+				return v
+			default:
+				return fmt.Sprintf("%v", v)
+			}
+		} else {
+			return "?"
+		}
+	}
+	buff := new(bytes.Buffer)
+	err = t.Funcs(fm).Execute(buff, data)
+	return buff.Bytes(), err
+}
+
 // Execute a template at the given uri/url.  The data to be applied to the template should
 // be placed in the context via the ContextPutTemplateData() function.
 func Execute(ctx context.Context, uri string, funcs ...template.FuncMap) ([]byte, error) {
@@ -63,27 +147,9 @@ func Execute(ctx context.Context, uri string, funcs ...template.FuncMap) ([]byte
 		url = string(applied)
 	}
 
-	var body []byte
-	var err error
-	switch {
-	case strings.Index(url, "func://") == 0:
-		if f, has := fm[url[len("func://"):]]; !has {
-			return nil, ErrMissingTemplateFunc
-		} else {
-			switch f.(type) {
-			case func() []byte:
-				body = f.(func() []byte)()
-			case func() string:
-				body = []byte(f.(func() string)())
-			default:
-				return nil, ErrBadTemplateFunc
-			}
-		}
-	default:
-		body, err = resource.Fetch(ctx, url)
-		if err != nil {
-			return nil, err
-		}
+	body, err := resource.Fetch(ctx, url)
+	if err != nil {
+		return nil, err
 	}
-	return Apply(body, data, fm)
+	return Apply2(body, data, fm)
 }
--- pkg/template/template_test.go
+++ pkg/template/template_test.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"encoding/json"
 	"github.com/conductant/gohm/pkg/auth"
 	"github.com/conductant/gohm/pkg/resource"
 	"github.com/conductant/gohm/pkg/server"
@@ -31,8 +32,8 @@ func (suite *TestSuiteTemplate) SetUpSuite(c *C) {
 		WithAuth(server.Auth{VerifyKeyFunc: testutil.PublicKeyFunc}.Init()).
 		Route(server.Endpoint{UrlRoute: "/secure", HttpMethod: server.GET, AuthScope: server.AuthScope("secure")}).
 		To(func(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
-		resp.Write([]byte(suite.template))
-	}).Start()
+			resp.Write([]byte(suite.template))
+		}).Start()
 }
 
 func (suite *TestSuiteTemplate) TearDownSuite(c *C) {
@@ -63,4 +64,36 @@ func (suite *TestSuiteTemplate) TestTemplateExecute(c *C) {
 	c.Assert(err, IsNil)
 	c.Log(string(text))
 	c.Assert(string(text), Equals, "My name is test and I am 20 years old.")
+}
+
+func (suite *TestSuiteTemplate) TestApply2(c *C) {
+
+	body := `
+{{define "app"}}
+version: 1.2
+image: repo
+build: 1234
+{{end}}
+
+{{define "host"}}
+label: appserver
+name: myhost
+{{end}}
+
+{
+   "image" : "repo/myapp:` + "{{my `app.version`}}-{{my `app.build`}}" + `",
+   "host" : "` + "{{my `host.name`}}" + `"
+}
+`
+
+	result, err := Apply2([]byte(body), nil)
+	c.Assert(err, IsNil)
+
+	c.Log(string(result))
+
+	obj := make(map[string]string)
+	err = json.Unmarshal(result, &obj)
+	c.Assert(err, IsNil)
+	c.Assert(obj["image"], Equals, "repo/myapp:1.2-1234")
+	c.Assert(obj["host"], Equals, "myhost")
 }
