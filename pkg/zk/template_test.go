fe5b51774ae61ab0e481e21e71b411e7bb630ac4
David Chung <davidchung@Davids-MacBook-Pro-2.local>
2016-03-07 15:22:50 -0800

    Added test for template define var blocks

--- pkg/zk/template_test.go
+++ pkg/zk/template_test.go
@@ -1,6 +1,7 @@
 package zk
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/conductant/gohm/pkg/auth"
 	"github.com/conductant/gohm/pkg/namespace"
@@ -11,6 +12,7 @@ import (
 	"golang.org/x/net/context"
 	. "gopkg.in/check.v1"
 	"net/http"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -180,4 +182,83 @@ func (suite *TestSuiteTemplate) TestListDeref(c *C) {
 		"host-1",
 		"host-0",
 	})
+}
+
+func (suite *TestSuiteTemplate) TestTemplateExecuteWithVarBlock(c *C) {
+	// Write something
+	url := "zk://" + strings.Join(Hosts(), ",")
+	zk, err := namespace.Dial(context.Background(), url)
+	c.Assert(err, IsNil)
+	c.Log(zk)
+
+	k := "/unit-test/registry/template/test-template-execute-vars/PG_PASS"
+	p := namespace.NewPath(k)
+
+	err = zk.Delete(p)
+
+	// Write new data
+	v := []byte("password")
+	_, err = zk.Put(p, v, false)
+	c.Assert(err, IsNil)
+
+	zk.Close()
+
+	// Now use the value in zk in the template
+
+	// Generate the auth token required by the server.
+	token := auth.NewToken(1*time.Hour).Add("secure", 1)
+	header := http.Header{}
+	token.SetHeader(header, testutil.PrivateKeyFunc)
+
+	// This is the content to be served by the test server.
+	// Using the Execute will add additional functions that can be included in the var blocks.
+	suite.template = `
+{{define "comments"}}
+# The variable define blocks allow the definition of variables that are reused throughout the
+# main body of the template.  The variables are referenced as '<blockname>.<fieldname>'.
+{{end}}
+
+{{define "app"}} # blockname is 'app'
+version: 1.2
+image: repo
+build: 1234
+user: "{{env "USER"}}"  # Getting the environment variable and use that as value.
+password: "{{ get "zk:///unit-test/registry/template/test-template-execute-vars/PG_PASS" }}"
+{{end}}
+
+{{define "host"}}
+label: appserver
+name: myhost
+port: {{.port}}  # Here we allow the application to pass in a context that's refereceable.
+{{end}}
+
+{
+   "image" : "repo/myapp:` + "{{my `app.version`}}-{{my `app.build`}}" + `",
+   "host" : "` + "{{my `host.name`}}" + `",{{/* use this for comment in JSON :) */}}
+   "user" : "` + "{{my `app.user`}}" + `",
+   "password" : "` + "{{my `app.password`}}" + `",
+   "port" : "` + "{{my `host.port`}}" + `"
+}`
+
+	// The url is a template too.
+	url = "http://localhost:{{.port}}/secure"
+
+	data := map[string]interface{}{
+		"Name": "test",
+		"Age":  20,
+		"port": suite.port,
+	}
+	ctx := template.ContextPutTemplateData(resource.ContextPutHttpHeader(context.Background(), header), data)
+
+	text, err := template.Execute(ctx, url)
+	c.Assert(err, IsNil)
+	c.Log(string(text))
+	obj := make(map[string]string)
+	err = json.Unmarshal(text, &obj)
+	c.Assert(err, IsNil)
+	c.Assert(obj["image"], Equals, "repo/myapp:1.2-1234")
+	c.Assert(obj["user"], Equals, os.Getenv("USER"))
+	c.Assert(obj["host"], Equals, "myhost")
+	c.Assert(obj["port"], Equals, fmt.Sprintf("%d", suite.port))
+	c.Assert(obj["password"], Equals, string(v))
 }
