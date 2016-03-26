f87c4f841cf77a2571da94fe2d6f96a6b852607d
David Chung <davidchung@Davids-MBP-2.lan>
2016-03-26 16:03:43 -0700

    Update env.sh for better handling of workspaces and GOPATH

--- hack/env.sh
+++ hack/env.sh
@@ -3,32 +3,29 @@
 : ${PROJECT:=`basename $PWD`}
 echo "Current directory is $(pwd). Project=${PROJECT}"
 
-if [ ! -d "$HOME/go/src/$PROJECT" ]; then
-    echo "Creating $HOME/go as the root of go development and set up symlinks to point to this directory."
-    IFS='/' read -a proj <<< "$PROJECT"
-    mkdir -p $HOME/go/src/${proj[0]}/${proj[1]}
-    ln -s $(pwd) $HOME/go/src/${proj[0]}/${proj[1]}/${proj[2]}
-fi
-
-export GOPATH=$HOME/go
-export PATH=$HOME/go/bin:$PATH
+# Set the GOPATH
+# The convention is that somewhere in the path there's a go/src/ directory.  We will set
+# the root of that as the GOPATH
+export GOPATH="$(pwd | awk -F '/go/src/' '{print $1}')/go"
+export PATH=$GOPATH/bin:$PATH
 
 # Godep dependency manager
 if [[ $(which godep) == "" ]]; then
     echo "Installing godep."
-    go get github.com/tools/godep
+    go install github.com/tools/godep
+else
+    echo "Found godep"
 fi
 
-# Development tools
-if [[ "$CIRCLE_ENV" != "" ]]; then
-    echo "Setting up development environment."
-    # Run go oracle for development golang.org/x/tools/oracle
-    if [[ $(which oracle) == "" ]]; then
-	echo "Setting up go oracle for source code analysis."
-	go install golang.org/x/tools/oracle
-    fi
-
-    if [[ $(which godoc) == "" ]]; then
-	echo "Godoc not installed."
-    fi
+if [[ $(which oracle) == "" ]]; then
+    echo "Setting up go oracle for source code analysis."
+    go install golang.org/x/tools/oracle
+else
+    echo "Found go oracle"
 fi
+
+echo "GOPATH=${GOPATH}"
+echo "PATH=${PATH}"
+echo "GO Binary: $(which go)"
+go version
+
